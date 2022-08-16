96b586fefc32d298593e506b24f6e7a126216fa0
kingraino93 <76224373+kingraino93@users.noreply.github.com>
2022-08-16 01:37:03 -0400

    section2

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Require needed modules
+require('dotenv').config()
+
 const express = require ('express')
 const app = express()
+
+app.get ('/search', function (req, res) {
+    console.log("QUERY PARAMS", req.query)
+    res.send('RestaurantPage')
+})
+
 app.get ('/', function (req, res) {
+    console.log(req.query)
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT, function () {
+    console.log("we are alive!!! http://localhost:3000")
+})
